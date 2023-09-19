ab5e6c09452ea7acd091ec12f6e7997da180aa95
Bharathraja2803 <bharathrajakrishnan@gmail.com>
2023-09-19 13:12:14 +0530

    after changing source of every question to a single source of array Arrays.stream(studentArray)

--- src/dev/Main.java
+++ src/dev/Main.java
@@ -2,6 +2,7 @@ package dev;
 
 import java.nio.file.DirectoryStream.Filter;
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
@@ -12,24 +13,19 @@ public class Main {
 
         Course pymc= new Course("PYMC", "Python Masterclass");
         Course jmc= new Course("JMC", "Java Masterclass");
-//        Student tim = new Student("AU", 2019, 30, "M",
-//                true, jmc, pymc);
-//        System.out.println(tim);
-//
-//        tim.watchLecture("JMC", 10, 5, 2019);
-//        tim.watchLecture("PYMC", 7, 7, 2020);
-//        System.out.println(tim);
+
+        Student[] studentArray = new Student[1000];
+        Arrays.setAll(studentArray, i -> Student.getRandomStudent(jmc, pymc));
+       
         System.out.println("=".repeat(80));
-        var genderCounting = Stream.generate(() -> Student.getRandomStudent(jmc, pymc))
-                .limit(20)
+        		var genderCounting = Arrays.stream(studentArray)
                 .collect(Collectors.groupingBy(Student::getGender,Collectors.counting()))
                 ;
         
         genderCounting.forEach((k, v) -> System.out.println(k+":"+v));
         
         System.out.println("=".repeat(80));
-        var ageListing = Stream.generate(() -> Student.getRandomStudent(jmc, pymc))
-                .limit(20)
+        var ageListing = Arrays.stream(studentArray)
                 .collect(Collectors.groupingBy(s -> {
                 	if(s.getAge() < 30) {
                 		return "Less Than 30";
@@ -45,8 +41,7 @@ public class Main {
         
         System.out.println("=".repeat(80));
         System.out.println("Distinct Countries of studnets entrolled: ");
-        Stream.generate(() -> Student.getRandomStudent(jmc, pymc))
-                .limit(20)
+        Arrays.stream(studentArray)
                 .map(Student::getCountryCode)
                 .distinct()
                 .forEach(System.out::println);
@@ -60,9 +55,8 @@ public class Main {
         System.out.println("Age Statistics: "+ageStatistics);
        
         System.out.println("=".repeat(80));
-        var stillActive = Stream.generate(() -> Student.getRandomStudent(jmc,pymc))
-         		.limit(20)
-         		.filter(s -> s.getYearEnrolled() <= 2015) 
+        var stillActive = Arrays.stream(studentArray)
+         		.filter(s -> s.getYearEnrolled() <= 2016) 
          		//.peek(System.out::println)
          		.anyMatch(s ->  s.getEngagementMap().values().stream()
          				//.peek(System.out::println)
@@ -73,14 +67,14 @@ public class Main {
         
         System.out.println("=".repeat(80));
         System.out.println("Top 5 Student who enrolled 6 years back and learning till now ");
-        Stream.generate(() -> Student.getRandomStudent(jmc,pymc))
+        Arrays.stream(studentArray)
         		.limit(100)
         		.filter(s -> s.getYearEnrolled() <= 2015)
 //        		.filter( c -> c.getEngagementMap().values().stream().anyMatch(v-> v.getLastActivityYear() == LocalDate.now().getYear()))
         		.filter(s -> s.getEngagementMap()
         						.values()
         						.stream()
-        						.anyMatch(c -> c.getLastActivityYear()==2023)
+        						.anyMatch(c -> c.getLastActivityYear()==LocalDate.now().getYear())
         						)
         		
         		.limit(5)
@@ -88,16 +82,7 @@ public class Main {
 					
        
         
-//        var maleStream = allStudentStream
-                //.forEach(System.out::println);
-                //.filter(s -> s.getGender() == "M")
-                //.collect(Collectors.groupingBy(Student::getGender))
-//                .filter(s -> s.getGender() == "M");
-            
-       //var femaleStream = allStudentStream.filter(s -> s.getGender() == "F");
-          
-         //  System.out.println("Male Students Count: "+maleStream.count());
-           //System.out.println("Female Students Count: "+femaleStream.count());
+
                 
         
     }
